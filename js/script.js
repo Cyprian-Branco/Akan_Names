ce745459b4f0b22070bb8235965740875d352765
Cyprian Bowen <bowencyprian@gmail.com>
2019-08-16 16:24:32 +0300

    give an alert if the input data is outside the range

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ function readInput(){
     month = parseInt(document.getElementById("month").value);
     date = parseInt(document.getElementById("date").value);
  
+//Give alert if the input data is outside the range
+    if (century < 0 || century >= 22){
+        alert("Enter a valid century");
+        return false;
+    } else if ( year < 0 || year >= 2020){
+        alert("Enter a valid year")
+        return false;
+    } else if (month < 0 || month > 12){
+        alert("Enter a valid month number");
+        return false;
+    } else if (date < 0 || date >31){
+        alert("Enter a valid date");
+    }
+
+}
