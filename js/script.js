90d8ed8823bfd86f0a1fe56b519b85c4a843166f
Cyprian Bowen <bowencyprian@gmail.com>
2019-08-19 10:02:16 +0300

    fix error

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function outputNames(){
     var month=parseFloat(document.getElementById("Month").value);
     var year=parseFloat(document.getElementById("Year").value);
     //Give alert if the input data is outside the range
-    if ( year < 0 || year > 2020){""
+    if ( year < 0 || year > 2020){
         alert("Enter a valid year")
         return false;
     } else if (month < 0 || month > 12){
